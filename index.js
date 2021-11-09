ee0063603ddc6cd8cd6facd2bd38ac3c8f12f7cb
RudeSniper122 <85300776+RudeSniper122@users.noreply.github.com>
2021-11-09 12:07:01 +0530

    fadad

---
+++ index.js
@@ -0,0 +1,17 @@
+function myFunction1() {
+  // document.getElementById('c2').classList.remove("show");
+  // document.getElementById('c3').classList.remove("show");
+  document.getElementById("c1").classList.toggle("show");
+}
+
+function myFunction2() {
+  // document.getElementById('c1').classList.remove("show");
+  // document.getElementById('c3').classList.remove("show");
+  document.getElementById('c2').classList.toggle("show");
+}
+
+function myFunction3() {
+  // document.getElementById('c1').classList.remove("show");
+  // document.getElementById('c2').classList.remove("show");
+  document.getElementById('c3').classList.toggle("show");
+}
